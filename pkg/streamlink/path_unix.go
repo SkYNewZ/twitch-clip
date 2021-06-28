f055beb7f7d3c73969989e4e21bf1cdf2774c9c9
Quentin Lemaire <quentin@lemairepro.fr>
2021-06-28 21:56:06 +0200

    fix: PATH workaround only for unix distros

---
+++ pkg/streamlink/path_unix.go
@@ -0,0 +1,11 @@
+// +build !windows
+
+package streamlink
+
+import "os"
+
+func init() {
+	// Append some paths in PATH (https://stackoverflow.com/questions/27451697/not-able-to-execute-go-file-using-os-exec-package)
+	origin := os.Getenv("PATH")
+	_ = os.Setenv("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:"+origin)
+}
