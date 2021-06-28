976cbd2849c0137c41471f95ce7818437323a98b
Quentin Lemaire <quentin@lemairepro.fr>
2021-06-28 23:50:27 +0200

    feat: Windows artifact is okay

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+GO="go.exe"
 VERSION=$(git describe --tags --exact-match 2>/dev/null || git describe --tags 2>/dev/null || echo "v0.0.0-$(git rev-parse --short HEAD)")
-export VERSION
+GOOS=$($GO env GOOS)
+GOARCH=$($GO env GOARCH)
+
+echo "Installing go-winres"
+$GO install github.com/tc-hib/go-winres@latest
 
 echo "Generating Windows manifests"
+export VERSION
 go generate ./...
 
 echo "Building Go app"
-go.exe build -ldflags="-X 'main.twitchClientID=${TWITCH_CLIENT_ID}' -X 'main.twitchClientSecret=${TWITCH_SECRET_ID}' -H=windowsgui" -tags production -o out/twitch-clip.exe .
+$GO build -ldflags="-X 'main.twitchClientID=${TWITCH_CLIENT_ID}' -X 'main.twitchClientSecret=${TWITCH_SECRET_ID}' -H=windowsgui" \
+  -tags production \
+  -o "out/twitch_clip_${VERSION}_${GOOS}_${GOARCH}.exe" .
